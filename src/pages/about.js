57352204d9ccf4f2aca74b937f42ad199601384e
shenghong777 <53395152+shenghong777@users.noreply.github.com>
2019-08-10 18:17:11 +0800

    Create about.js

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => (
+  <div>
+    <h1>About Us</h1>
+    <p>
+      I am super awesome. Call me the best in the world. Testing 123. Hahaha. Too easy. Millionair incoming, go away coperate life.
+    </p>
+  </div>
+)
+
+export default AboutPage
