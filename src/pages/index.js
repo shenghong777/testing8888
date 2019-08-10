31cafcfcf0309d6ac3a9e5e7de7829b4847d5fa6
shenghong777 <53395152+shenghong777@users.noreply.github.com>
2019-08-10 18:14:49 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,15 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import React from 'react'
+import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>sssss</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <div>
+    <h1>Welcome to my Domain</h1>
+    <p>Bow down to my Awesomeness!!</p>
+   // <Link to="/page-2/"
+    //style={{
+      //color: "purple",
+      //textDecoration: "underline"}}> Go to next page </Link>
+  </div>
 )
 
 export default IndexPage
