79fe33294b5ebaa8e70a0f922af60934eb6ccc06
shenghong777 <53395152+shenghong777@users.noreply.github.com>
2019-08-10 18:27:04 +0800

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,36 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React from 'react'
+import Link from 'gatsby-link'
 
 const Header = ({ siteTitle }) => (
-  <header
+  <div
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      background: '#F8ca9d',
+      marginBottom: '0',
     }}
   >
     <div
       style={{
-        margin: `0 auto`,
+        margin: '0 auto',
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: '1.45rem 1.0875rem',
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `white`,
-            textDecoration: `none`,
+            color: 'white',
+            textDecoration: 'no',
+            textShadow: '5px 5px black',
+            webkitTextStrokeWidth: '1.2px',
+            webkitTextStrokeColor: '#FF5733',        
           }}
         >
           {siteTitle}
         </Link>
       </h1>
     </div>
-  </header>
+  </div>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
